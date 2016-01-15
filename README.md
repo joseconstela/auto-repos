172cc2ddefe5d8a8e5475e815d6055f700d649aa
jrcconstela <jose@joseconstela.com>
2016-01-15 17:52:39 +0100

    docs

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
 # zsh auto-repo's plugin
 
-Aliases to help you to manage large development projects with many repositories. It runs git/npm commands in subfolders depending on each subfolder's nature.
+Aliases to help you to manage large projects with many git, nodejs or bower apps directories.
+
+It basically runs commands in all subfolders depending on each directory's nature. 
 
 Originally designed as an [oh-my-zsh plugin](https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins)
 
-# Aliases
+## Common commands. Runs in all sulfolders.
+* `ara <command>` runs `<command>`
 
-* `ara <command>` runs `<command>` in any subfolders
-* `arn <command>` runs `<command>` in nodejs subfolders
-* `arg <command>` runs `<command>` in gits subfolders
+## Git commands. Runs in subfolders with a .git/config file
+* `arg <command>` runs `<command>`
+* `argp` runs `git pull`
+* `argco <branch>` runs `git checkout` to the specified branch
 
-# Other aliases
+## Bower commands. Runs in subfolders with a bower.json file
+* `arb <command>` runs `<command>`
+* `arbi` runs `bower install`
+* `arbu` runs `bower update`
 
+## NodeJS commands. Runs in subfolders with a packages.json file
+* `arn <command>` runs `<command>`
 * `arni` runs `npm install`
 * `arnu` runs `npm update`
-* `argp` runs `git pull`
-* `arnl github` grabs the list of package.json dependencies, and runs `npm link <depName>` where its name or repo's url matches `github` - in the current folder
-* `arnla github` same as `arnl` but executes in all subfolders
-* `argco <branch>` runs `git checkout` to the specified branch
+* `arnla <match>` grabs the list of package.json dependencies, and where it maches `match`, executes `npm link <the package name>`
+* `arnl github` same as `arnla` but only in current directory
+
+### Example
+For all nodejs subfolders, created an npm link for each one of them, and link them according to repositories matching "github" in both the name or url in packages.json
+
+`arn npm link; arnla github`
--- auto-repos.plugin.zsh
+++ auto-repos.plugin.zsh
@@ -1,17 +1,24 @@
+# MIT license.
+# gihub: @joseconstela
+
+# Stores current directory for later use
 _arSavePwd() {
     _ARPWD=`pwd`
 }
 
+# cds into previous directory
 _arRollBackPwd() {
     cd $_ARPWD;
 }
 
+# Executes the arguments in the current directory
 _arAnyAction() {
     echo "Folder" `pwd` "..."
     echo "  ^ " $@
     $@
 }
 
+# Executes the arguments in the current directory - if .git/config file exists
 _arGitAction() {
     echo "Folder" `pwd` "..."
     if [ -f ".git/config" ]; then
@@ -22,6 +29,7 @@ _arGitAction() {
     fi
 }
 
+# Executes the arguments in the current directory - if package.json file exists
 _arNodeAction() {
     echo "Folder" `pwd` "..."
     if [ -f "package.json" ]; then
@@ -32,6 +40,7 @@ _arNodeAction() {
     fi
 }
 
+# Executes the arguments in the current directory - if bower.json file exists
 _arBowerAction() {
     echo "Folder" `pwd` "..."
     if [ -f "bower.json" ]; then
@@ -42,56 +51,86 @@ _arBowerAction() {
     fi
 }
 
-_arNi() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction npm install; done;
-    _arRollBackPwd
-}
-
-_arNu() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction npm update; done;
-    _arRollBackPwd
-}
-
-_arGp() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arGitAction git pull; done;
-    _arRollBackPwd
-}
-
-_arGco() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arGitAction git checkout $@; done;
-    _arRollBackPwd
-}
-
+# Loops subfolders and executues parameters in all subfolders
 _arA() {
     _arSavePwd
     find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arAnyAction $@; done;
     _arRollBackPwd
 }
 
-_arN() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction $@; done;
-    _arRollBackPwd
-}
-
-_arB() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arBowerAction $@; done;
-    _arRollBackPwd
-}
-
+# Loops subfolders and executues parameters in git subfolders
 _arG() {
     _arSavePwd
     find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arGitAction $@; done;
     _arRollBackPwd
 }
 
-_arnl() {
+# Loops subfolders and executues git pull in git subfolders
+_arGp() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arGitAction git pull; done;
+    _arRollBackPwd
+}
 
+# Loops subfolders and executues git checkout in git subfolders
+_arGco() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arGitAction git checkout $@; done;
+    _arRollBackPwd
+}
+
+# Loops subfolders and executues parameters in bower subfolders
+_arB() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arBowerAction $@; done;
+    _arRollBackPwd
+}
+
+# Loops subfolders and executues bower install in bower subfolders
+_arBi() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arBowerAction bower install; done;
+    _arRollBackPwd
+}
+
+# Loops subfolders and executues bower update in bower subfolders
+_arBu() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arBowerAction bower update; done;
+    _arRollBackPwd
+}
+
+# `arn <command>` runs `<command>`
+_arN() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction $@; done;
+    _arRollBackPwd
+}
+
+# `arni` runs `npm install`
+_arNi() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction npm install; done;
+    _arRollBackPwd
+}
+
+# `arnu` runs `npm update`
+_arNu() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arNodeAction npm update; done;
+    _arRollBackPwd
+}
+
+# `arnla <match>` grabs the list of package.json dependencies, and where 
+# it maches `match`, executes `npm link <the package name>`
+_arnla() {
+    _arSavePwd
+    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arnl $@; done;
+    _arRollBackPwd
+}
+
+# same as `arnla` but only in current directory
+_arnl() {
     echo "Folder" `pwd` "..."
 
     if [ -f "package.json" ]; then
@@ -107,20 +146,25 @@ _arnl() {
     fi
 }
 
-_arnla() {
-    _arSavePwd
-    find . -maxdepth 1 -type d | while read -r line; do cd "$_ARPWD/$line" && _arnl $@; done;
-    _arRollBackPwd
-}
+## Common commands. Runs in all sulfolders.
+alias ara=_arA              # `ara <command>` runs `<command>`
 
-alias arni=_arNi
-alias arnu=_arNu
-alias argp=_arGp
-alias argco=_arGco
-alias arnl=_arnl
-alias arnla=_arnla
+## Git. Runs in subfolders with a .git/config file
+alias arg=_arG              # `arg <command>` runs `<command>`
+alias argp=_arGp            # `argp` runs `git pull`
+alias argco=_arGco          # `argco <branch>` runs `git checkout` to 
+                            # the specified branch
 
-alias ara=_arA
-alias arn=_arN
-alias arb=_arB
-alias arg=_arG
+## Bower. Runs in subfolders with a bower.json file
+alias arb=_arB              # `arb <command>` runs `<command>`
+alias arbi=_arBi            # `arbi` runs `bower install`
+alias arbu=_arBu            # `arbu` runs `bower update`
+
+## NodeJS. Runs in subfolders with a packages.json file
+alias arn=_arN              # `arn <command>` runs `<command>`
+alias arni=_arNi            # `arni` runs `npm install`
+alias arnu=_arNu            # `arnu` runs `npm update`
+alias arnla=_arnla          # `arnla <match>` grabs the list of package.json 
+                            # dependencies, and where it maches `match`,
+                            # executes `npm link <the package name>`
+alias arnl=_arnl            # same as `arnla` but only in current directory
